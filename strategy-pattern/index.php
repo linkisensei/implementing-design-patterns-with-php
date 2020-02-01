bae6958c6fe76e0f0cdc8189eb0be2975f95c2c1
linkisensei <lucas.b.fisica@gmail.com>
2020-02-01 10:04:49 -0300

    Added a simple implementation of the Strategy Pattern

---
+++ strategy-pattern/index.php
@@ -0,0 +1,129 @@
+<?php
+/*
+
+ * Define a family of algorithms, encapsulate each one, and make them interchangeable. Strategy lets the algorithm vary independently from the clients that use it.
+ 
+ * Capture the abstraction in an interface, bury implementation details in derived classes
+
+ */
+
+
+/* Main Class  */
+class SimpleApiConsumer {
+    protected $parser;
+    protected $printer;
+    protected $url;
+
+    function __construct($parser, $printer, $url) {
+        $this->parser = $parser;
+        $this->printer = $printer;
+        $this->url = $url;
+    }
+
+    public function execute() {
+        try {
+            $response = file_get_contents($this->url);
+
+            $parsed = $this->parser->parse($response);
+
+            if($parsed !== false) {
+                $this->printer->print($parsed);
+            }
+
+        } catch (\Throwable $th) {
+            print_r($th);
+        }
+    }
+
+    public changeStrategies($new_parser, $new_printer) {
+        $this->parser = $new_parser;
+        $this->printer = $new_printer;
+    }
+}
+
+
+/** Strategies */
+interface ParserStrategy {
+    public function parse($raw, $to_associative_array);
+}
+
+class JsonParserStrategy implements ParserStrategy {
+    public function parse($raw, $to_associative_array = false) {
+
+        if(!mb_detect_encoding($raw, 'UTF-8', true) ) {
+            $raw = utf8_encode($raw);
+        }
+        $parsed = json_decode($raw, $to_associative_array);
+        if (!!$parsed) {
+            return $parsed;
+        } else {
+            return false;
+        }
+    }
+}
+
+class XmlParserStrategy implements ParserStrategy {
+    public function parse($raw, $to_associative_array = false) {
+        
+        return simplexml_load_string($raw);  
+    }
+}
+
+
+interface Printer {
+    public function  print($input);
+}
+
+class PrintToScreen {
+    public function  print($input) {
+        print_r($input);
+    }
+}
+
+class PrintToFile {
+    private $_path;
+
+    function __construct($path) {
+        $this->_path = $path;
+    }
+
+    public function  print($input) {
+        $output = print_r($input, true);
+        file_put_contents($this->_path, $output);
+    }
+}
+
+
+
+/* Testind some objects */
+
+// A consumer that gets a json and displays it on the screen:
+
+$json_to_screen = new SimpleApiConsumer(
+                                new JsonParserStrategy(),
+                                new PrintToScreen(),
+                                'https://trpg.ga/api/bestiario/index.php'
+                            );
+
+$json_to_screen->execute();
+
+// A consumer that gets a json and dumps it to a file:
+
+$json_to_file = new SimpleApiConsumer(
+                                new JsonParserStrategy(),
+                                new PrintToFile('./json_to_file.log'),
+                                'https://trpg.ga/api/bestiario/index.php'
+                            );
+
+$json_to_file->execute();
+
+
+// A consumer that gets a XML and dumps it to a file:
+
+$json_to_file = new SimpleApiConsumer(
+                                new XmlParserStrategy(),
+                                new PrintToFile('./xml_to_file.log'),
+                                'http://www.deviante.com.br/podcasts/sociedade-brasileira-de-nefrologia/feed/'
+                            );
+
+$json_to_file->execute();
